889f08133a436d112e24c9a75259e571887acc18
Anton Kravchenko <antonio57banderas@gmail.com>
2022-04-15 01:46:21 +0300

    added keeper contract and updated price feeder

--- smart_contracts/MathContract.sol
+++ smart_contracts/MathContract.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
-
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity ^0.8.0;
 
 contract MathContract{
 
-    function sumArr(uint256[] memory nums) internal pure returns(uint256){
+
+    function sumArr(uint256[2] memory nums) external pure returns(uint256){
         uint256 _sum = 0;
         for(uint8 i; i < nums.length; i++){
             _sum += nums[i];
@@ -12,49 +12,24 @@ contract MathContract{
         return _sum;
     }
 
-    function ln(uint256 num) internal pure returns(uint256){
-        return log2(num) * 693 / 1000;
-    }
+    function expABC(
+        uint256 _a,
+        uint256 _b,
+        uint256 _c,
+        uint8 seriesLength
+    ) external pure returns(uint256){
 
-    function log2(uint256 num) internal pure returns(uint256){
-        return intPart(num);
-    }
+        uint256 _result;
+        uint256 _factorial = 1;
 
-
-    function intPart(uint256 x) internal pure returns(uint256){
-        uint256 int_part = 0;
-        if (x >= 2**128) {
-            x >>= 128;
-            int_part += 128;
+        for(uint8 i = 0; i <= seriesLength; i++){
+            if(i == 0){
+                _result += _a;
+            } else {
+                _result += ((_a * _b ** i) / (_c ** i * _factorial));
+                _factorial *= (i + 1);
+            }
         }
-        if (x >= 2**64) {
-            x >>= 64;
-            int_part += 64;
-        }
-        if (x >= 2**32) {
-            x >>= 32;
-            int_part += 32;
-        }
-        if (x >= 2**16) {
-            x >>= 16;
-            int_part += 16;
-        }
-        if (x >= 2**8) {
-            x >>= 8;
-            int_part += 8;
-        }
-        if (x >= 2**4) {
-            x >>= 4;
-            int_part += 4;
-        }
-        if (x >= 2**2) {
-            x >>= 2;
-            int_part += 2;
-        }
-        if (x >= 2**1) {
-            int_part += 1;
-        }
-
-        return int_part;
+        return _result;
     }
 }
---
+++ smart_contracts/PriceFeeder.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+import "https://github.com/smartcontractkit/chainlink/blob/develop/contracts/src/v0.8/ChainlinkClient.sol";
+
+contract PriceFeeder is ChainlinkClient {
+    using Chainlink for Chainlink.Request;
+
+    uint256 public price;
+    string assetName;
+
+    address private oracle;
+    bytes32 private jobId;
+    uint256 private fee;
+
+    constructor(string memory _assetName) {
+        setPublicChainlinkToken();
+        oracle = 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8;
+        jobId = "d5270d1c311941d0b08bead21fea7747";
+        assetName = _assetName;
+        fee = 0.1 * 10 ** 18;
+    }
+
+    function requestVolumeData() public returns (bytes32 requestId)
+    {
+        Chainlink.Request memory request = buildChainlinkRequest(
+            jobId,
+            address(this),
+            this.fulfill.selector
+        );
+
+        request.add("get", getUrl());
+
+        request.add("path", assetName);
+
+        request.add("path", "usd");
+
+        int timesAmount = 10**18;
+        request.addInt("times", timesAmount);
+
+        return sendChainlinkRequestTo(oracle, request, fee);
+    }
+
+    function fulfill(bytes32 _requestId, uint256 _price) public
+    {
+       price = _price;
+    }
+
+    function getUrl() internal view returns (string memory) {
+        string memory startUrl = "https://api.coingecko.com/api/v3/simple/price?ids=";
+        string memory endUrl = "&vs_currencies=usd";
+        return string(abi.encodePacked(startUrl, assetName, endUrl));
+    }
+}
