62d3fdb4c8d171d078a60627013265192b7d781b
Кравченко Антон <antonio57banderas@gmail.com>
2022-09-16 12:27:28 +0200

    decreased money requirement

--- pythia-frontend/src/config.js
+++ pythia-frontend/src/config.js
@@ -523,9 +523,9 @@ export const payTokenAddress = '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063';
 export const chain = 'polygon';
 export const chainId = "0x89";
 
-export const minSharesCreate = 4;
-export const minMoneyCreate = 4;
+export const minSharesCreate = 2;
+export const minMoneyCreate = 2;
 
-export const minMoney = 2;
+export const minMoney = 1;
 
 export const incorrectChainMessage = 'Incorrect network: please switch to Polygon Main Network';
